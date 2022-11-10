b0badf0af281c0a78396288778dc309ab4fa445f
13-Rob <rcgr.13@gmail.com>
2022-11-09 17:20:10 -0800

    Revert "Se agrego mongoose"
    This reverts commit 6dd79c67782a790455b9b8659db0329ede1b4b1b.

--- src/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect('mongodb://localhost/difer', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(db => console.log('Ya esta conectado'))
-.catch((err) => console.error(err));
